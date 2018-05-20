22c3246ff5a0a4cf371135e8596733f0897ac0a1
nadira.nuri <nadira.nuri@gmail.com>
2018-05-20 17:01:21 +0600

    t 3 20180520 pharma

--- www/survey.js
+++ www/survey.js
@@ -1525,8 +1525,8 @@ function check_user() {
 	
 	
 	
-	var  apipath_base_photo_dm ='http://w02.yeapps.com/ipinmd/syncmobile_417_new_ibn_newtest/dmpath?CID='+cid +'&HTTPPASS=e99business321cba'
-	//var  apipath_base_photo_dm ='http://w02.yeapps.com/ipi/syncmobile_417_new_ibn_newtest/dmpath?CID='+cid +'&HTTPPASS=e99business321cba'
+	//var  apipath_base_photo_dm ='http://w02.yeapps.com/ipinmd/syncmobile_417_new_ibn_newtest/dmpath?CID='+cid +'&HTTPPASS=e99business321cba'
+	var  apipath_base_photo_dm ='http://w02.yeapps.com/ipi/syncmobile_417_new_ibn_newtest/dmpath?CID='+cid +'&HTTPPASS=e99business321cba'
 	
 	//var  apipath_base_photo_dm='http://a007.yeapps.com/acme/syncmobile_417_new/dmpath?CID='+cid +'&HTTPPASS=e99business321cba'
 
